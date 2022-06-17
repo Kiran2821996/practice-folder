eb8545bab05ae2992681c8f428f86464d8a313fa
kiran kumar <kirandchennai@gmail.com>
2022-06-17 15:09:56 +0530

    inverted pattern

---
+++ 17.06.2022/Inverted triangle pattern/script.js
@@ -0,0 +1,8 @@
+//printing inverted right angle triangle
+let rowi = 10;
+for (let i = rowi; i >=1 ; i--) {
+  for (let j = i; j >= 1; j--) {
+    document.write("*");
+  }
+  document.write("<br/>");
+}

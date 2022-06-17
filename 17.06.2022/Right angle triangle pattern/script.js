1b3a0c13184fa1b6dc0a7d3d9e5f0036e6ef108e
kiran kumar <kirandchennai@gmail.com>
2022-06-17 14:35:41 +0530

    rightAngle pattern

---
+++ 17.06.2022/Right angle triangle pattern/script.js
@@ -0,0 +1,8 @@
+// printing star in righ angle triangle
+let rowr = 10;
+for (let i = 1; i <= rowr; i++) {
+  for (let j = 1; j <= i; j++) {
+    document.write("*");
+  }
+  document.write("<br/>");
+}

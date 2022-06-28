032b695eaee19d4b74f7a8d9e709f495df0c9ceb
kiran kumar <kirandchennai@gmail.com>
2022-06-28 17:36:10 +0530

    pattern in terminal

---
+++ 28.06.2022/readme.md
@@ -0,0 +1,8 @@
+<h1>print * sqaure pattern</h1>
+<img src="./images/Screenshot (209).png" alt="">
+<h1>print * triangle pattern</h1>
+<img src="./images/Screenshot (210).png" alt="">
+<h1>print * number series pattern</h1>
+<img src="./images/Screenshot (211).png" alt="">
+<h1>print * number pattern</h1>
+<img src="./images/Screenshot (212).png" alt="">
---
+++ 28.06.2022/script.js
@@ -0,0 +1,39 @@
+//print * sqaure pattern
+// let num=5;
+// for(let i=0;i<num;i++){
+//     let star=""
+//     for(let j=0;j<=num;j++){
+//         star= star+"*"
+//     }
+//     console.log(star);
+// }
+
+//print * in right angle triangle
+// let num=5;
+// for(let i=1;i<=5;i++){
+//   let star=""
+//   for(let j=1;j<=i;j++){
+//     star=star+"*"
+//   }
+//   console.log(star)
+// }
+
+//print numbers series in right angle triangle
+// let num=5;
+// for(let i=1;i<=5;i++){
+//   let star=""
+//   for(let j=1;j<=i;j++){
+//     star=star+j
+//   }
+//   console.log(star)
+// }
+
+//print numbers in right angle triangle
+let num=5;
+for(let i=1;i<=5;i++){
+  let star=""
+  for(let j=1;j<=i;j++){
+    star=star+i
+  }
+  console.log(star)
+}

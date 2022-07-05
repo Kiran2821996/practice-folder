24e086f7b04b368c3e0b8d2ccf93dc0b02bae0cf
kiran kumar <kirandchennai@gmail.com>
2022-07-05 15:35:29 +0530

    arrray

---
+++ 05.07.2022/array.js
@@ -0,0 +1,24 @@
+//Counting number of letters in pharse
+
+// const howMany=(pharse)=>{
+// for( letter of pharse){}
+// console.log(pharse.length)
+// }
+// howMany("hai,My name is kiran")
+
+// Counting number of digits in given array
+// const howMany=(Numbers)=>{
+//     for( num of Numbers){}
+//     console.log(Numbers.length)
+//     }
+//     howMany([1,2,3,4,5])
+
+
+//Counting number of digits in given array with respect to index
+const howMany=(Numbers)=>{
+    for( num in Numbers){}
+    console.log(num)
+    }
+    howMany([1,2,3,4,5])
+
+
---
+++ 05.07.2022/readme.md
@@ -0,0 +1,6 @@
+<h1>Output1</h1>
+<img src="./images/Screenshot (251).png" alt="">
+<h1>Output2</h1>
+<img src="./images/Screenshot (252).png" alt="">
+<h1>Output3</h1>
+<img src="./images/Screenshot (253).png" alt="">

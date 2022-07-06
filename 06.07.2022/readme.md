54953a19945315ca3cce1ffdc477a2f5e55c0e26
kiran kumar <kirandchennai@gmail.com>
2022-07-06 12:18:49 +0530

    arry methods

---
+++ 06.07.2022/readme.md
@@ -0,0 +1,10 @@
+<h1>push</h1>
+<img src="./images/Screenshot (255).png" alt="">
+<h1>pop</h1>
+<img src="./images/Screenshot (256).png" alt="">
+<h1>shift</h1>
+<img src="./images/Screenshot (257).png" alt="">
+<h1>splice</h1>
+<img src="./images/Screenshot (258).png" alt="">
+<h1>slice</h1>
+<img src="./images/Screenshot (259).png" alt="">
---
+++ 06.07.2022/script.js
@@ -0,0 +1,21 @@
+//Array methods
+
+//push
+// arry.push("avengers")
+// console.log(arry);
+//pop
+// arry.pop()
+// console.log(arry);
+//shift
+// console.log( arry.shift(), arry.shift());
+// console.log(arry);
+//unshift
+// arry.unshift({person:"Dr.Strange",activity:"TimeTraveller"})
+// console.log(arry)
+//splice
+// arry.splice(1,2,{person:"Dr.Strange",activity:"TimeTraveller"})
+// console.log(arry)
+//slice
+const arry=[1,2,3,"name",{person:"thor"},{activity:"destroyer"}]
+console.log(arry.slice(0,2)); 
+console.log(arry)

8bb6e9299578e0de7a34303d1744df7551e982ac
kiran kumar <kirandchennai@gmail.com>
2022-07-07 17:41:54 +0530

    include

---
+++ 07.07.2022/script.js
@@ -0,0 +1,35 @@
+// const arr=[2,4,5,5,6,7,7,8,9,9,2,4]
+
+// console.log(arr.includes(2,arr.length-1))
+
+//First repeating number one another the other
+// const arr1=[]
+// for(let i=0;i<arr.length;i++ ){
+//     if(arr1.includes(arr[i])){
+//         console.log(arr[i]);
+//     break
+//     } else{
+//         arr1.push(arr[i])
+//     }
+// }
+//First repeating number
+
+// for(let i=0;i<arr.length;i++ ){
+//     let value=arr[i]
+//     if(arr.includes(value,i+1)===true){
+//         console.log(value);
+//     break
+//     } 
+// }
+
+//first non repeating number
+// let arr = [10, 5,5, 3, 4, 3, 7, 6];
+// for(let i=0;i<arr.length;i++){
+//     for(let j=0;j<arr.length;j++){
+//         if(arr[i]===arr[j+1]){
+//             console.log(arr[i]);
+//             break ;
+//         }
+//     }
+// }
+

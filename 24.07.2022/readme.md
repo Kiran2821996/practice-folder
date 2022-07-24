1ad2b3b6899f8852d79d86969697e136432e792f
kiran kumar <kirandchennai@gmail.com>
2022-07-24 18:27:12 +0530

    practice

---
+++ 24.07.2022/readme.md
@@ -0,0 +1,2 @@
+<h1>HIGER ORDER FUNCTION</h1>
+<img src="./images/Screenshot (38).png" alt="">
---
+++ 24.07.2022/s.js
@@ -0,0 +1,24 @@
+//HIGER ORDER FUNCTION
+// const radius = [1,2,3,4,5]
+
+// const area = function(radius){
+//   return Math.floor(Math.PI*Math.pow(radius,2)) 
+// }
+// const circumference = function(radius){
+//   return Math.floor(2*Math.PI^radius) 
+// }
+// const diameter = function(radius){
+//   return Math.floor(2*radius)
+// }
+// const calculate = function(raw,logic){
+//   let output = []
+//   for(let i in raw){ 
+//     output.push(logic(raw[i]))
+//   }
+//   return output
+// }
+
+// console.log(calculate(radius,area))
+// console.log(calculate(radius,circumference))
+// console.log(calculate(radius,diameter))
+

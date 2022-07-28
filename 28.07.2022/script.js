9bc24b6d4fad9a79c0c9bb8e2905c9040070c046
kiran kumar <kirandchennai@gmail.com>
2022-07-28 13:20:49 +0530

    DOM assignment

---
+++ 28.07.2022/script.js
@@ -0,0 +1,28 @@
+//<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<Question 1>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+document.querySelector(".class1").innerText="B D Kiran Kumar"
+//<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<Question 2>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+document.querySelector(".class2").innerText="Myself Kiran from Puducherry(U.T),done Btech mechanical engineering,persuing Full stack Developer course in AirCampus"
+//<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<Question 3>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+document.querySelector(".class3").style.color="red"
+//<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<Question 4>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+document.querySelectorAll(".divs")[0].style.border="solid black"
+document.querySelectorAll(".divs")[1].style.border="solid black"
+document.querySelectorAll(".divs")[2].style.border="solid black"
+document.querySelectorAll(".divs")[0].innerText="Fourth"
+document.querySelectorAll(".divs")[1].innerText="Fifth"
+document.querySelectorAll(".divs")[2].innerText="Sixth"
+//<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<Question 5>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+const node = document.createElement("span");
+const textnode = document.createTextNode("Aircampus");
+node.appendChild(textnode);
+document.getElementById("id1").appendChild(node);
+//<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<Question 6>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+document.querySelectorAll("div")[8].innerHTML="<div class='new_htlm'> <p>This is my first assignment</p> </div> "
+//<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<Question 7>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+document.querySelectorAll("div")[10].innerHTML="<h6>My Experience with Aircampus</h6>"
+document.querySelectorAll("div")[10].style.textDecoration="underline"
+document.querySelectorAll("div")[10].style.fontWeight="bold"
+document.querySelectorAll("div")[10].style.fontSize="32px"
+document.querySelectorAll("div")[10].style.color="orange"
+//<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<Question 8>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+document.querySelectorAll("p")[1].innerText="My experience with Aircampus is been so attached with my logical thinking and as well in improving the communication skills."

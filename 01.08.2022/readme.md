3066bf130a61894236eba9710cf96c75d813b5f0
kiran kumar <kirandchennai@gmail.com>
2022-08-01 18:46:39 +0530

    buttons

---
+++ 01.08.2022/readme.md
@@ -0,0 +1,2 @@
+<h1>output</h1>
+<video src="./video/final_62e7d090add80e0068d079a9_778105.mp4"></video>
---
+++ 01.08.2022/script.js
@@ -0,0 +1,40 @@
+function change1() {
+  document.querySelector(".p1").classList.toggle("red");
+  if (document.querySelector(".p1").innerText == "Hai iam Black") {
+    document.querySelector(".p1").innerText = "Hai iam red";
+  } else {
+    document.querySelector(".p1").innerText = "Hai iam Black";
+  }
+}
+function change2() {
+  document.querySelector(".p2").classList.toggle("green");
+  if (document.querySelector(".p2").innerText == "Hai iam yellow") {
+    document.querySelector(".p2").innerText = "Hai iam green";
+  } else {
+    document.querySelector(".p2").innerText = "Hai iam yellow";
+  }
+}
+function change3() {
+  document.querySelector(".p3").classList.toggle("pink");
+  if (document.querySelector(".p3").innerText == "Hai iam violet") {
+    document.querySelector(".p3").innerText = "Hai iam pink";
+  } else {
+    document.querySelector(".p3").innerText = "Hai iam violet";
+  }
+}
+function change4() {
+  document.querySelector(".p4").classList.toggle("blue");
+  if (document.querySelector(".p4").innerText == "Hai iam brown") {
+    document.querySelector(".p4").innerText = "Hai iam blue";
+  } else {
+    document.querySelector(".p4").innerText = "Hai iam brown";
+  }
+}
+function change5() {
+  document.querySelector(".p5").classList.toggle("black");
+  if (document.querySelector(".p5").innerText == "Hai iam orange") {
+    document.querySelector(".p5").innerText = "Hai iam black";
+  } else {
+    document.querySelector(".p5").innerText = "Hai iam orange";
+  }
+}
